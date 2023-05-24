e7017da35434f6af07c8df8a27aa65eeea8cf44e
22004743 <132383000+DangYanRou@users.noreply.github.com>
2023-05-24 12:36:36 +0800

    Update ReadPNG.java

--- ReadPNG.java
+++ ReadPNG.java
@@ -6,45 +6,19 @@ import javax.imageio.ImageIO;
 
 public class ReadPNG
 {
-    public static void main (String [] args) throws IOException 
+    private File file;
+    private int [][] image_dots;
+    private int [][] image_pixels;
+ 
+    public ReadPNG(File file) throws IOException
     {
-        // Load the PNG file
-        File file1 = new File("D:\\DOWNLOAD\\image 1.png");
-        File file2 = new File("D:\\DOWNLOAD\\image 2.png");
-        File file3 = new File("D:\\DOWNLOAD\\image 3.png");
-        File file4 = new File("D:\\DOWNLOAD\\image 4.png");
-        
-        System.out.println("Pixels of image 1");
-        int [][] image1_pixels = scanImage(file1);
-        System.out.println();
-        int [][] image1_dots = converting(image1_pixels);
-        System.out.println();
-        
-        System.out.println("Pixels of image 2");
-        int [][] image2_pixels = scanImage(file2);
-        System.out.println();
-        int [][] image2_dots = converting(image2_pixels);
-        System.out.println();
-        
-        System.out.println("Pixels of image 3");
-        int [][] image3_pixels = scanImage(file3);
-        System.out.println();
-        int [][] image3_dots = converting(image3_pixels);
-        System.out.println();
-        
-        System.out.println("Pixels of image 4");
-        int [][] image4_pixels = scanImage(file4);
-        System.out.println();
-        int [][] image4_dots = converting(image4_pixels);
-        System.out.println();
-        
-        
-        
-        
+        this.file=file;
+        this.image_pixels=this.scanImage(file);
+        this.image_dots=this.converting();
     }
     
-    
-    public static int [][] scanImage (File f) throws IOException
+   
+    public int [][] scanImage (File f) throws IOException
     {
         BufferedImage image = ImageIO.read(f);
         
@@ -66,40 +40,50 @@ public class ReadPNG
 
                 // Store the pixel values
                 pixels[y][x]=green;
+
                 
-                //Display
-                System.out.print(pixels[y][x] + " ");
             }
-            System.out.println();
+           
         }
         return pixels;
     }
     
-    public static int[][] converting(int[][] pixels) 
+    public int[][] converting() 
     {
-        int [][] dots = new int [pixels.length][];
+        int [][] dots = new int [this.image_pixels.length][];
         
-        for (int i=0; i<pixels.length; i++)
+        for (int i=0; i<this.image_pixels.length; i++)
         {
-            dots[i]=new int[pixels[i].length];
+            dots[i]=new int[this.image_pixels[i].length];
             
-            for (int j=0; j<pixels[i].length;j++)
+            for (int j=0; j<this.image_pixels[i].length;j++)
             {
                 //converting
-                if (pixels[i][j]==0)
+                if (this.image_pixels[i][j]==0)
                     dots[i][j]=0;
-                else if (pixels[i][j]==64)
+                else if (this.image_pixels[i][j]==64)
                     dots[i][j]=1;
-                else if (pixels[i][j]==128)
+                else if (this.image_pixels[i][j]==128)
                     dots[i][j]=2;
-                else if (pixels[i][j]==192)
+                else if (this.image_pixels[i][j]==192)
                     dots[i][j]=3;
                 
-                System.out.print(dots[i][j]+" ");
+                
             }
-            System.out.println();
+         
         }
         
         return dots;
     }
+    
+    public int[][] getArrayDots()
+    { 
+        return this.image_dots;
+    }
+    
+    public int [][] getArrayPixels()
+    {
+        return this.image_pixels;
+    }
 }
+
